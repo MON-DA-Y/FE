3e98c6b98131e09ca21e0e3f108579e57adda5be
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-05-13 19:15:59 +0900

    Setting/#1 : 디자인 시스템 설정 (tailwind.config.ts, tokens.ts, globals.css)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,57 +1,80 @@
 import type { Config } from "tailwindcss";
+import { COLORS, FONT_SIZE, FONT_WEIGHT, LETTER_SPACING, LINE_HEIGHT, SHADOW } from "./src/styles/theme/tokens";
 
 const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors: {
-        // Primary
-        navy: "#1C376B",
-        mint: "#A0EBAF",
-
-        // Sub
-        black: "#1C1C1C",
-        gray1: "#EFEFEF",
-        gray2: "#D9D9D9",
-        gray3: "#8E8E8E",
-        gray4: "#636363",
-
-        // 시리즈, 퀴즈
-        yellow1: "#FFFD79",
-        yellow2: "#FDE68A",
-        yellow3: "#92400D",
-        blue1: "#EFF6FF",
-        blue2: "#C5DFFF",
-        blue3: "#1D40B0",
-        deepGreen: "#166434",
-        error: "#F66C6C",
-
-        // 카테고리
-        money: "#F6CA83",
-        bigPicture: "#D0D38F",
-        dailyLife: "#9EA96B",
-        global: "#B1DCDD",
-        industries: "#8193B6",
-        tech: "#AFAFAF",
-        issues: "#FABEC0",
-        rules: "#E97B76",
-      },
-
       fontSize: {
-        headline: "35px",
-        subtitle1: "30px",
-        subtitle2: "20px",
-        body1: "17px",
-        body2: "15px",
-        caption1: "12px",
-        caption2: "12px",
+        headline: [
+          FONT_SIZE.headline,
+          {
+            lineHeight: LINE_HEIGHT.headline,
+            fontWeight: FONT_WEIGHT.headline,
+            letterSpacing: LETTER_SPACING.headline,
+          },
+        ],
+        subtitle1: [
+          FONT_SIZE.subtitle1,
+          {
+            lineHeight: LINE_HEIGHT.subtitle1,
+            fontWeight: FONT_WEIGHT.subtitle1,
+            letterSpacing: LETTER_SPACING.subtitle1,
+          },
+        ],
+        subtitle2: [
+          FONT_SIZE.subtitle2,
+          {
+            lineHeight: LINE_HEIGHT.subtitle2,
+            fontWeight: FONT_WEIGHT.subtitle2,
+            letterSpacing: LETTER_SPACING.subtitle2,
+          },
+        ],
+        body1: [
+          FONT_SIZE.body1,
+          {
+            lineHeight: LINE_HEIGHT.body1,
+            fontWeight: FONT_WEIGHT.body1,
+            letterSpacing: LETTER_SPACING.body1,
+          },
+        ],
+        body2: [
+          FONT_SIZE.body2,
+          {
+            lineHeight: LINE_HEIGHT.body2,
+            fontWeight: FONT_WEIGHT.body2,
+            letterSpacing: LETTER_SPACING.body2,
+          },
+        ],
+        caption1: [
+          FONT_SIZE.caption1,
+          {
+            lineHeight: LINE_HEIGHT.caption1,
+            fontWeight: FONT_WEIGHT.caption1,
+            letterSpacing: LETTER_SPACING.caption1,
+          },
+        ],
+        caption2: [
+          FONT_SIZE.caption2,
+          {
+            lineHeight: LINE_HEIGHT.caption2,
+            fontWeight: FONT_WEIGHT.caption2,
+            letterSpacing: LETTER_SPACING.caption2,
+          },
+        ],
       },
-
+      colors: {
+        ...COLORS.primary,
+        ...COLORS.sub,
+        ...COLORS.series,
+        ...COLORS.category,
+      },
       boxShadow: {
-        clickable: "0 2px 6px rgba(0, 0, 0, 0.15)",
+        interactive: SHADOW.interactive,
       },
     },
   },
   plugins: [],
 };
+
 export default config;
