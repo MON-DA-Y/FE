32b577b6fb334e597fa84564f515b7921a5b2eeb
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-05-13 19:16:41 +0900

    Setting/#1: layout 기본 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <p>root</p>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <h1 className="text-headline">HEADLINE</h1>
+      <p className="text-body2">본문 내용입니다.</p>
+      <button className="bg-[#1C376B] ">안녕</button>
     </div>
   );
 }
