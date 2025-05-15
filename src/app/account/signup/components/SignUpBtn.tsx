61d13c3973f32eb4f724eaf9ad41657fd4aa2615
Lee Hye Won <thankyou@sookmyung.ac.kr>
2025-05-15 22:02:03 +0900

    feat/#3: signupsuccess page 구현

--- src/app/account/signup/components/SignUpBtn.tsx
+++ src/app/account/signup/components/SignUpBtn.tsx
@@ -1,12 +1,26 @@
+"use client";
+
 import { COLORS, FONT_SIZE, FONT_WEIGHT, SHADOW } from "@/styles/theme/tokens";
+import { useRouter } from "next/navigation";
 
 interface SignUpBtnProps {
   userType: "student" | "parent";
 }
 
 export default function SignUpBtn({ userType }: SignUpBtnProps) {
+  const router = useRouter();
+
+  const handleClick = () => {
+    if (userType === "student") {
+      router.push("/account/signupsuccess/student");
+    } else {
+      router.push("/account/signupsuccess/parent");
+    }
+  };
+
   return (
     <div
+      onClick={handleClick}
       className="flex items-center justify-center w-[284px] h-[56px] border text-white"
       style={{
         backgroundColor:
