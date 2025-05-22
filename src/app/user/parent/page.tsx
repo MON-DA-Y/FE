aa57cf4ca4efcd74bc2dc0c295de7c9b398f9116
Lee Hye Won <thankyou@sookmyung.ac.kr>
2025-05-22 16:32:48 +0900

    feat/#10: StudentSchool component 생성

--- src/app/user/parent/page.tsx
+++ src/app/user/parent/page.tsx
@@ -1,6 +1,9 @@
-import StudentProfile from "@/app/components/StudentProfile";
+import StudentProfile from "@/app/user/components/StudentProfile";
 import ParentProfile from "./components/ParentProfile";
-import StudentLevel from "@/app/components/StudentLevel";
+import StudentLevel from "@/app/user/components/StudentLevel";
+import { FONT_SIZE, FONT_WEIGHT } from "@/styles/theme/tokens";
+import Image from "next/image";
+import StudentSchool from "./components/StudentSchool";
 
 export default function ParentPage() {
   return (
@@ -8,11 +11,49 @@ export default function ParentPage() {
       <div className="pl-180">
         <ParentProfile />
       </div>
-      <div className="flex items-start">
+
+      <div className="flex flex-row items-start gap-9">
         <div className="flex flex-col items-center">
           <StudentProfile width={100} height={100} />
           <div className="-mt-4">
             <StudentLevel />
+          </div>
+        </div>
+
+        <div className="flex flex-col pt-3">
+          <div
+            className="flex flex-row gap-2.5 items-center"
+            style={{
+              fontSize: FONT_SIZE.headline,
+              fontWeight: FONT_WEIGHT.headline,
+            }}
+          >
+            이00
+            <StudentSchool />
+          </div>
+          <div
+            className="flex flex-row gap-1.5 pt-4"
+            style={{ fontSize: FONT_SIZE.body2, fontWeight: FONT_WEIGHT.body2 }}
+          >
+            <Image
+              src="/icons/Calendar.svg"
+              alt="calendar"
+              width={24}
+              height={24}
+            />
+            가입일 : 2025.02.14
+          </div>
+          <div
+            className="flex flex-row gap-1.5"
+            style={{ fontSize: FONT_SIZE.body2, fontWeight: FONT_WEIGHT.body2 }}
+          >
+            <Image
+              src="/icons/Edit_Pencil.svg"
+              alt="pencil"
+              width={24}
+              height={24}
+            />
+            총 학습일 : 63일
           </div>
         </div>
       </div>
