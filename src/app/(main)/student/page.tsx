7463e65a80c27b14998bae23adc0b9cad478e739
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-07-20 00:37:09 +0900

    Chore, Design/#6: student main page 에 각 컴포넌트 추가 및 디자인 수정

--- src/app/(main)/student/page.tsx
+++ src/app/(main)/student/page.tsx
@@ -3,16 +3,22 @@
 import Logo from "./components/Logo";
 import MonWord from "./components/MonWord";
 import MonSeries from "./components/MonSeries/MonSeries";
+import MonNews from "./components/MonNews";
+import TodayLearningRate from "./components/TodayLearningRate";
 import { useRouter } from "next/navigation";
 
 export default function StudentMyPage() {
   const router = useRouter();
 
   return (
-    <div className="relative w-full h-full overflow-auto px-13 pt-5">
+    <div className="relative w-full h-full overflow-auto px-10 pt-5">
       <Logo />
       <MonWord />
       <MonSeries />
+      <div className="absolute bottom-0 right-5 flex flex-col items-center gap-[15px]">
+        <TodayLearningRate />
+        <MonNews />
+      </div>
     </div>
   );
 }
