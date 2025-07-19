357429e3c37bc67e74de7adf6bbdce92de9750d5
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-07-19 22:28:27 +0900

    Feat/#6: SeriesCard 구현

---
+++ src/app/(main)/student/components/MonSeries/SeriesCard.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { useState } from "react";
+import { COLORS, FONT_SIZE, FONT_WEIGHT } from "@/styles/theme/tokens";
+import WordBox from "@/components/ui/WordBox";
+
+interface SeriesCardProps {
+  onClick: () => void;
+  category: string;
+  children: React.ReactNode;
+}
+export default function SeriesCard({
+  onClick,
+  category,
+  children,
+}: SeriesCardProps) {
+  return (
+    <>
+      <div className="mt-4 relative">
+        <div className="left-6 top-[-10] absolute">
+          <WordBox
+            p={"3px 10px"}
+            bg={`${COLORS.primary.mint}`}
+            textColor={`${COLORS.series.deepGreen}`}
+            font={"caption1"}
+          >
+            {category}
+          </WordBox>
+        </div>
+        <div
+          onClick={onClick}
+          className="z-10 w-80 h-20 px-10 py-3 bg-white rounded-[30px] shadow-[0px_0px_10px_0px_rgba(0,0,0,0.25)] inline-flex justify-start items-center gap-5 "
+        >
+          <div
+            className="w-56 justify-start truncate overflow-hidden whitespace-nowrap"
+            style={{
+              color: COLORS.primary.navy,
+              fontSize: FONT_SIZE.subtitle2,
+              fontWeight: FONT_WEIGHT.subtitle2,
+            }}
+          >
+            {children}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
