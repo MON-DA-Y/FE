4a6ffc424a087a6b428c1346752527a0319f2fec
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-07-21 01:45:10 +0900

    Feat/#24: WordItem 구현

---
+++ src/app/(main)/MonWord/components/WordItem.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import NavyBox from "@/components/ui/NavyBox";
+import CommonBtn from "@/components/shared/CommonBtn";
+import { COLORS, FONT_SIZE, FONT_WEIGHT } from "@/styles/theme/tokens";
+
+export interface WordItemProps {
+  id: number;
+  word: string;
+  explain: React.ReactNode;
+  use: string;
+}
+
+export default function WordItem({ id, word, explain, use }: WordItemProps) {
+  const handleUnderstandClick = (id: number) => {
+    console.log(`${id}에 해당하는 단어 이해했어요`);
+  };
+
+  return (
+    <div className="w-[680px] px-10 pt-10 pb-7 bg-white rounded-[30px] flex flex-col gap-5">
+      {/* 단어 및 설명 */}
+      <div className="flex flex-col gap-4" style={{ color: COLORS.sub.black }}>
+        <div
+          style={{
+            fontSize: FONT_SIZE.subtitle2,
+            fontWeight: FONT_WEIGHT.subtitle2,
+          }}
+        >
+          {word}
+        </div>
+        <div
+          style={{ fontSize: FONT_SIZE.body2, fontWeight: FONT_WEIGHT.body2 }}
+        >
+          {explain}
+        </div>
+      </div>
+
+      {/* 예문 (써먹기) */}
+      <NavyBox title="써먹기">{use}</NavyBox>
+
+      {/* 버튼 */}
+      <div className="flex justify-end">
+        <CommonBtn
+          type="understand"
+          onClick={() => handleUnderstandClick(id)}
+        />
+      </div>
+    </div>
+  );
+}
