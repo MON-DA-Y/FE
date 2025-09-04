5a5d98718a52262a9f140585c8de38fb49a68f31
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-09-04 22:22:18 +0900

    fix/#48: WordItem 로직 수정 및 이해했어요 API 연동

--- src/app/(main)/MonWord/components/WordList.tsx
+++ src/app/(main)/MonWord/components/WordList.tsx
@@ -50,6 +50,22 @@ export default function WordList() {
     }
   };
 
+  // mon단어 item 이해했어요 post
+  const postTodayMonWordItemUnderstand = async (wordId: number) => {
+    try {
+      setIsLoading(true);
+      const data = await monWordApi.postWordItemUnderstand(wordId);
+      console.log(data);
+    } catch (error: any) {
+      if (error.response) {
+        alert(error.response.data.message);
+      } else {
+        alert("알 수 없는 오류가 발생했습니다.");
+      }
+      console.error("오늘의 monWord 학습 완료 post 실패", error);
+    }
+  };
+
   const handleFinishClick = () => {
     postTodayMonWordDone();
   };
@@ -64,6 +80,7 @@ export default function WordList() {
             word={item.word}
             explain={item.explain}
             use={item.use}
+            understandFunc={() => postTodayMonWordItemUnderstand(item.id)}
           />
         ))}
         <div className="">
