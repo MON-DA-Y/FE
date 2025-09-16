e65d953b5bb90e03118c179bcf020a8f235793df
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-09-16 19:05:02 +0900

    feat/#65: 학생 메인 monWord 조회 api 연동

---
+++ src/apis/stdMain.ts
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+// 토큰 가져오기
+const getToken = () => {
+  const token = localStorage.getItem("token");
+  //   console.log("가져온 토큰:", token);
+  return token;
+};
+
+// 토큰을 헤더에 추가
+const getAuthHeader = () => {
+  const token = getToken();
+  const headers = {
+    Authorization: `Bearer ${token}`,
+  };
+  //   console.log("생성된 헤더:", headers);
+  return headers;
+};
+
+export const stdMainApi = {
+  // monWord 조회
+  getStdMonWord: async () => {
+    try {
+      const headers = getAuthHeader();
+      const response = await axios.get(`${baseURL}/std/monWord`, {
+        headers,
+      });
+      // console.log("monWord 조회 결과:", response.data);
+      return response.data.result;
+    } catch (error) {
+      console.error("monWord get 실패:", error);
+      throw error;
+    }
+  },
+};
