3673c440bb73c8025602e39236834bed970b3270
Lee Hye Won <thankyou@sookmyung.ac.kr>
2025-09-19 03:13:48 +0900

    feat/#74: 학부모 이름 연결

--- src/app/user/parent/components/ParentProfile.tsx
+++ src/app/user/parent/components/ParentProfile.tsx
@@ -1,12 +1,24 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import { FONT_SIZE, FONT_WEIGHT } from "@/styles/theme/tokens";
 import Image from "next/image";
 import Link from "next/link";
+import { getParentInfo, PrtInfoResponse } from "@/apis/parentInfo";
 
 export default function ParentProfile() {
+  const [parent, setParent] = useState<PrtInfoResponse | null>(null);
+
+  useEffect(() => {
+    getParentInfo()
+      .then(setParent)
+      .catch((err) => console.error("부모 정보 API 실패:", err));
+  }, []);
+
   return (
     <div className="flex items-center gap-7">
       <div style={{ fontSize: FONT_SIZE.body2, fontWeight: FONT_WEIGHT.body2 }}>
-        이00님
+        {parent?.prt_name}님
       </div>
       <div>
         <Link href="../parent/mypage" className="cursor-pointer">
