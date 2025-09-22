5071705098ffc7d4932f346944aa8b60c526b80f
Lee Hye Won <thankyou@sookmyung.ac.kr>
2025-09-23 02:56:13 +0900

    feat/#93: parent 컬렉션에 studentId 추가

--- src/apis/parentInfo.ts
+++ src/apis/parentInfo.ts
@@ -15,3 +15,27 @@ export async function getParentInfo(): Promise<PrtInfoResponse> {
   console.log("API response:", res.data.result);
   return res.data.result;
 }
+
+// 부모에게 자녀 추가
+interface AddStudentRequest {
+  studentId: string;
+}
+
+interface AddStudentResponse {
+  message: string;
+  student: {
+    id: string;
+    name: string;
+    school: string;
+    grade: string;
+  };
+}
+
+export async function addStudent(
+  studentId: string
+): Promise<AddStudentResponse> {
+  const reqBody: AddStudentRequest = { studentId }; // 인터페이스 사용
+  const res = await API.post<AddStudentResponse>("/add-student", reqBody);
+  console.log("자녀 추가 API response:", res.data);
+  return res.data;
+}
