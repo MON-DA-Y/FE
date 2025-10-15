d04946d2e4a727390afde223a638474650e3dd3d
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-10-15 13:02:12 +0900

    deploy: 배포를 위한 next.config 설정

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,26 @@
 import type { NextConfig } from "next";
 
+// // 로컬호스트 ver
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   // 외부 이미지 허용
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "**", // 모든 호스트 허용
+//       },
+//     ],
+//   },
+// };
+
+// 배포 ver
 const nextConfig: NextConfig = {
-  /* config options here */
-  // 외부 이미지 허용
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**", // 모든 호스트 허용
-      },
-    ],
+  output: "export",
+  image: {
+    unoptimized: true,
   },
+  trailingSlash: true,
 };
 
 export default nextConfig;
