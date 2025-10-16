22981ebd6cfbc920514b7e69b2f50881165d44d0
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-10-16 12:39:34 +0900

    fix: 홈화면 안보임 오류 해결

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,26 @@
 "use client";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
-
-  // const loginClick = () => {
-  //   router.push("/account/login");
-  // };
 
   const signupClick = () => {
     router.push("/account");
   };
 
   return (
-    <div className="relative flex justify-center">
+    <div className="relative flex justify-center gap-5">
       <div className="flex flex-col gap-0">
-        <div onClick={signupClick}>
-          <Image
-            src="/home/home1.svg"
-            alt="home1"
-            fill
-            className="object-contain"
-          />
-        </div>
-        <Image
-          src="/home/home2.svg"
-          alt="home2"
-          fill
-          className="object-contain"
+        <img
+          src="/home/home1.svg"
+          alt="home1"
+          className="w-full h-auto cursor-pointer"
+          onClick={signupClick}
         />
-        <Image
-          src="/home/home3.svg"
-          alt="home3"
-          fill
-          className="object-contain"
-        />
-        <Image
-          src="/home/home4.svg"
-          alt="home4"
-          fill
-          className="object-contain"
-        />
-        <Image
-          src="/home/home5.svg"
-          alt="home5"
-          fill
-          className="object-contain"
-        />
+        <img src="/home/home2.svg" alt="home2" className="w-full h-auto" />
+        <img src="/home/home3.svg" alt="home3" className="w-full h-auto" />
+        <img src="/home/home4.svg" alt="home4" className="w-full h-auto" />
+        <img src="/home/home5.svg" alt="home5" className="w-full h-auto" />
       </div>
     </div>
   );
