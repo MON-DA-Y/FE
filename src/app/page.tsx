4f7c4fd9b410b31f76c752b40ead7a349ae78cea
You Dongeun <130419669+uehddms@users.noreply.github.com>
2025-10-16 13:11:43 +0900

    design: 홈화면 스크롤 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,14 @@ export default function Home() {
   };
 
   return (
-    <div className="relative flex justify-center w-full gap-5">
-      <div className="flex flex-col gap-0">
+    <div className="relative flex justify-center w-100dvw gap-5">
+      <div
+        className="flex flex-col gap-0"
+        style={{
+          scrollbarWidth: "none", // Firefox
+          msOverflowStyle: "none", // IE/Edge
+        }}
+      >
         <img
           src="/home/home1.svg"
           alt="home1"
